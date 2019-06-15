904d5b34a97c7ef163912d35c6ceb8e504a55828
Andrew MacNaughton <andy@ziptrek.com>
2019-06-15 14:35:06 -0700

    updated the vlaue

--- src/js/autocomplete-vue.vue
+++ src/js/autocomplete-vue.vue
@@ -31,7 +31,7 @@
             @keydown.up.prevent.stop="moveUp()"
             @keydown.enter.prevent.stop="select(selectedIndex)"
             @keydown.tab="mousefocus = false"
-           
+            :value="value"
             ref="input"
             :required="required">
         <div v-if="showSuggestions" :class="classPrefix + '__suggestions'">
